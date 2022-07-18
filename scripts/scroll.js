b387c32b70b6d554a44492a2377731abac943707
Graham <graham@silcrow.co>
2022-07-18 13:04:12 +0000

    refactor: css tidied up to prep responsiveness

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -3,34 +3,23 @@ const blurbSection = document.querySelector(".intro-blurb")
 const blurbSquiggle = document.querySelector(".intro-blurb .squiggle")
 const pageBody = document.querySelector("body")
 
-// const calculateHeight = function() {
-// 	let vh = window.innerHeight * 0.01
-// 	document.documentElement.style.setProperty('--vh', `${vh}px`)
+// if (window.matchMedia('(min-width: 640px)').matches) {
+// 	console.log('Wide viewport');
+// } else {
+// 	console.log('Small viewport');
 // }
-// 
-// window.addEventListener('resize', () => {
-// 	if (window.pageYOffset <= 0) {
-//     	calculateHeight()
-// 	}
-// })
-// 
-// calculateHeight()
 
 const scrollEffects = function() {
-	// const yPosition = window.pageYOffset
-	// if (blurbSection.offsetTop - window.innerHeight <  yPosition){
+
 	if (window.pageYOffset > 0){
-		// titleSection.style.opacity = 0;
-		// titleSection.style.paddingTop = 0;
-		// titleSection.style.paddingBottom = 0;
-		titleSection.classList.add("collapse")
+		titleSection.classList.add("collapsed")
 		pageBody.classList.remove("reversed")
 	}
 	else {
-		// titleSection.style.opacity = 1;
-		titleSection.classList.remove("collapse")
+		titleSection.classList.remove("collapsed")
 		pageBody.classList.add("reversed")
 	}
+	
 }
 
 window.addEventListener('scroll', () => {
