15f82483e20c294bf64d9f288c85b962a14c2bdc
Graham <graham@silcrow.co>
2022-08-30 10:29:51 +0000

    refactor: make interaction code more readable

--- scripts/case-study-interaction.js
+++ scripts/case-study-interaction.js
@@ -4,23 +4,25 @@ const mainCta = document.querySelector(".show-main")
 const gallerySection = document.querySelector(".gallery")
 const mainSection = document.querySelector("main")
 
+
 const showGallery = function() {
 	gallerySection.classList.remove("hidden")
 	mainSection.classList.add("hidden")
 	gallerySection.scrollIntoView( {behavior: "smooth"} )
 }
 
-galleryCta.addEventListener("click", function() {
-	
-  showGallery()
-  
-})
-
 const showMain = function() {
 	gallerySection.classList.add("hidden")
 	mainSection.classList.remove("hidden")
 	mainSection.scrollIntoView( {behavior: "smooth"} )
 }
+
+
+galleryCta.addEventListener("click", function() {
+	
+  showGallery()
+  
+})
 
 mainCta.addEventListener("click", function() {
 	
