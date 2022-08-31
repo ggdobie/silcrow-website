37fcd7db344550710fe0fa360a4e6c24cce2923c
Graham <graham@silcrow.co>
2022-08-31 13:38:40 +0000

    feat: add scrolling badge to jointly banner

--- scripts/case-study-interaction.js
+++ scripts/case-study-interaction.js
@@ -1,31 +1,30 @@
 const galleryCta = document.querySelector(".show-gallery")
 const mainCta = document.querySelector(".show-main")
+const scrollBadge = document.querySelector(".scrollBadge")
+const firstSection = document.querySelector(".intro")
 
 const gallerySection = document.querySelector(".gallery")
 const mainSection = document.querySelector("main")
 
 
-const showGallery = function() {
-	gallerySection.classList.remove("hidden")
-	mainSection.classList.add("hidden")
-	gallerySection.scrollIntoView( {behavior: "smooth"} )
-}
-
-const showMain = function() {
-	gallerySection.classList.add("hidden")
-	mainSection.classList.remove("hidden")
-	mainSection.scrollIntoView( {behavior: "smooth"} )
-}
-
-
 galleryCta.addEventListener("click", function() {
 	
-  showGallery()
+  gallerySection.classList.remove("hidden")
+  mainSection.classList.add("hidden")
+  gallerySection.scrollIntoView( {behavior: "smooth"} )
   
 })
 
 mainCta.addEventListener("click", function() {
 	
-  showMain()
+  gallerySection.classList.add("hidden")
+  mainSection.classList.remove("hidden")
+  mainSection.scrollIntoView( {behavior: "smooth"} )
+  
+})
+
+scrollBadge.addEventListener("click", function() {
+	
+  firstSection.scrollIntoView( {behavior: "smooth"} )
   
 })
