fa50007b09df3b9d637f60ab4ced5a89591fcebe
Graham <graham@silcrow.co>
2022-10-05 12:24:33 +0000

    feat: improve colour contrast of hompeage elements

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,5 +1,8 @@
 const titleSection = document.querySelector(".intro-title")
 const blurbSection = document.querySelector(".intro-text")
+const silcrowIcon = document.querySelector(".silcrow-icon")
+const h1Tag = document.querySelector(".title-text h1")
+const h2Tag = document.querySelector(".title-text h2")
 const blurbSquiggle = document.querySelector(".intro-blurb .squiggle")
 const pageBody = document.querySelector("body")
 const contentContainer = document.querySelector(".content-container")
@@ -10,6 +13,9 @@ const scrollEffects = function() {
 		titleSection.classList.add("collapsed")
 		pageBody.classList.remove("reversed")
 		blurbSection.classList.remove("reversed")
+		silcrowIcon.classList.remove("reversed")
+		h1Tag.classList.remove("reversed")
+		h2Tag.classList.remove("reversed")
 		blurbSquiggle.classList.remove("reversed")
 		contentContainer.classList.remove("translucent")
 	}
@@ -17,6 +23,9 @@ const scrollEffects = function() {
 		titleSection.classList.remove("collapsed")
 		pageBody.classList.add("reversed")
 		blurbSection.classList.add("reversed")
+		silcrowIcon.classList.add("reversed")
+		h1Tag.classList.add("reversed")
+		h2Tag.classList.add("reversed")
 		blurbSquiggle.classList.add("reversed")
 		contentContainer.classList.add("translucent")
 	}
