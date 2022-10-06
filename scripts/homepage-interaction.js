3ff156432d9f9b0579577188d03548981b524f21
Graham <graham@silcrow.co>
2022-10-06 15:22:18 +0000

    feat: get colophon animation working

--- scripts/homepage-interaction.js
+++ scripts/homepage-interaction.js
@@ -2,18 +2,16 @@ const toggleSwitch = document.querySelector(".toggle-colophon")
 const toggleCaret = document.querySelector(".toggle-colophon .caret")
 const colophonTag = document.querySelector(".colophon")
 const footerTag = document.querySelector("footer")
-
+ 
 
 const toggleColophon = function() {
 	colophonTag.classList.toggle("hidden")
 	toggleCaret.classList.toggle("active")
 	footerTag.classList.toggle("expanded")
 
-	// setTimeout(function(){
-	// 	toggleSwitch.scrollIntoView({
-	// 	  behavior: 'smooth'
-	// 	})
-	// }, 300)
+	toggleSwitch.scrollIntoView({
+	  behavior: 'smooth'
+	})
 }
 
 toggleSwitch.addEventListener("click", function() {
