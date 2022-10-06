79390caa3d56c46a6d787da89fe4c163b6d2b03d
Graham <graham@silcrow.co>
2022-10-06 15:09:31 +0000

    feat: contextually scroll to bookmark from cta

--- scripts/case-study-interaction.js
+++ scripts/case-study-interaction.js
@@ -9,22 +9,39 @@ const firstSection = document.querySelector(".scrollTarget")
 
 const gallerySection = document.querySelector(".gallery")
 const mainSection = document.querySelector("main")
-// const bannerSection = document.querySelector(".banner")
 
+const bookmarkedSection = document.querySelector(".bookmark")
+const footerSection = document.querySelector("footer")
+
+let returnToBookmark = false
 
 const showMain = function () {
 
   gallerySection.classList.add("hidden")
   mainSection.classList.remove("hidden")
   bannerSection.classList.remove("hidden")
-  mainSection.scrollIntoView( {behavior: "smooth"} )
   
   galleryTab.classList.remove("selected")
   analysisTab.classList.add("selected")
   
+  if (footerSection.classList.contains("chameleon")) {
+    footerSection.classList.remove("paper")
+    footerSection.classList.add("tan")    
+  }
+  else {}
+  
+  // scroll down to the gallery cta if it was the one that triggered the switch to gallery
+  // otherwise scroll to the top
+  if (returnToBookmark == true) {
+    bookmarkedSection.scrollIntoView( {behavior: "smooth"} )
+  }
+  else {
+    mainSection.scrollIntoView( {behavior: "smooth"} )
+  }
+  
 }
 
-const showGallery = function () {
+const showGallery = function (clickedElement) {
   
   gallerySection.classList.remove("hidden")
   mainSection.classList.add("hidden")
@@ -34,14 +51,29 @@ const showGallery = function () {
   galleryTab.classList.add("selected")
   analysisTab.classList.remove("selected")
   
+  if (footerSection.classList.contains("chameleon")) {
+    footerSection.classList.add("paper")
+    footerSection.classList.remove("tan")    
+  }
+  else {}
+  
   scrollEffects()
+  
+  // check if clicked cta was further down the case study or the tab in the mode switcher
+  // if so, set the bookmark to return to later
+  if (clickedElement.target.classList.contains("cta")) {
+    returnToBookmark = true;
+  }
+  else {
+    returnToBookmark = false;
+  }
   
 }
 
 galleryCtas.forEach(cta => {
   
-  cta.addEventListener("click", function() {    
-    showGallery()
+  cta.addEventListener("click", function(EventTarget) {    
+    showGallery(EventTarget)
   })
   
 })
